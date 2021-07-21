2221e9d16f936fe6619c36edfc1758e074df747f
Ricardo Nogueira <ricardonogueirafr@outlook.fr>
2021-07-21 12:48:40 +0200

    [Add] checkSauceInput

---
+++ backend/middleware/checkSauceInput.js
@@ -0,0 +1,35 @@
+module.exports = (req, res, next) => {
+  if (JSON.parse(req.body.sauce !== undefined)) {
+    const sauce = JSON.parse(req.body.sauce);
+    let { name, manufacturer, description, mainPepper } = sauce;
+    let trimedTab = [];
+
+    function toTrim(...string) {
+      trimedTab = string.map((elt) => elt.trim());
+    }
+    toTrim(name, manufacturer, description, mainPepper);
+
+    const hasThreeCharacteres = (currentValue) => currentValue.length >= 3;
+    if (trimedTab.every(hasThreeCharacteres)) {
+      next();
+    } else {
+      throw new Error("Tous les champs doivent faire au moins 3 caractères");
+    }
+  } else {
+    const sauce = req.body;
+    let { name, manufacturer, description, mainPepper } = sauce;
+    let trimedTab = [];
+
+    function toTrim(...string) {
+      trimedTab = string.map((elt) => elt.trim());
+    }
+    toTrim(name, manufacturer, description, mainPepper);
+
+    const hasThreeCharacteres = (currentValue) => currentValue.length >= 3;
+    if (trimedTab.every(hasThreeCharacteres)) {
+      next();
+    } else {
+      throw new Error("Tous les champs doivent faire au moins 3 caractères");
+    }
+  }
+};
