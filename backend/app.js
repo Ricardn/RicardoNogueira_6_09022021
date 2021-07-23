711dca69b6b34380e59aed2c01ea0303775c250f
Ricardo Nogueira <ricardonogueirafr@outlook.fr>
2021-07-23 18:45:38 +0200

    [Fix] Remove unused Cors

--- backend/app.js
+++ backend/app.js
@@ -20,23 +20,9 @@ mongoose
 
 const app = express();
 
+app.use(helmet());
 
 //CORS (Cross-origin resource sharing)
-//Define access and permissions
-app.use(helmet());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
-
 app.use(cors());
 app.use(express.json());
 
